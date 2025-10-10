9b484fc63aa38fcf8d8414771848d3cbd1ad9905
Ayushi Sharma <ayushi.sharma@sambodhi.co.in>
2025-10-10 17:42:09 +0530

    analyzeblade

--- resources/views/chatbots/analyze.blade.php
+++ resources/views/chatbots/analyze.blade.php
@@ -25,23 +25,22 @@
                         <table id="datatable" class="table table-bordered dt-responsive nowrap w-100">
                             <thead>
                                 <tr>
-                                    <th>Name</th>
-                                    <th>Position</th>
-                                    <th>Office</th>
-                                    <th>Age</th>
-                                    <th>Start date</th>
-                                    <th>Salary</th>
+                                    @foreach($tableData as $botMessage => $userResponses)
+                                        <th>{{ $botMessage }}</th>
+                                    @endforeach
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td>Tiger Nixon</td>
-                                    <td>System Architect</td>
-                                    <td>Edinburgh</td>
-                                    <td>61</td>
-                                    <td>2011/04/25</td>
-                                    <td>$320,800</td>
-                                </tr>
+                                @php
+                                    $maxRows = max(array_map('count', $tableData));
+                                @endphp
+                                @for($i = 0; $i < $maxRows; $i++)
+                                    <tr>
+                                        @foreach($tableData as $userResponses)
+                                            <td>{{ $userResponses[$i] ?? '' }}</td>
+                                        @endforeach
+                                    </tr>
+                                @endfor
                             </tbody>
                         </table>
                     </div>
@@ -50,7 +49,14 @@
             <div class="tab-pane fade" id="nav-metrics" role="tabpanel" tabindex="0">
                 <div class="card-section-table">
                     <div class="card-body">
-                        <p>Metrics content goes here...</p>
+                        <h3>Generic Analytics</h3>
+                        <ul>
+                            <li>Total Conversations: {{ $analytics['total_conversations'] }}</li>
+                            <li>Total Messages: {{ $analytics['total_messages'] }}</li>
+                            <li>Bot Messages: {{ $analytics['bot_messages'] }}</li>
+                            <li>User Messages: {{ $analytics['user_messages'] }}</li>
+                            <li>Average User Messages per Conversation: {{ $analytics['average_user_messages_per_conversation'] }}</li>
+                        </ul>
                     </div>
                 </div>
             </div>
